343574f59fb609804e776c71d685c4e9cfa9ae9d
Daria <dasha.afanaseva.1206@mail.ru>
2026-03-14 20:05:42 +0300

    Completed Lab: Algorithms and Flowcharts

---
+++ code/Task1_BMI/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("-- КАЛЬКУЛЯТОР ИМТ --");
+
+Console.Write("Введите вес (кг): ");
+double ves = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите рост (м): ");
+double rost = Convert.ToDouble(Console.ReadLine());
+
+double imt = ves / (rost * rost);
+
+string kategoriya = "";
+string sovet = "";
+
+if (imt < 18.5)
+{
+    kategoriya = "Недостаточный вес";
+    sovet = "Нужно лучше питаться";
+}
+else if (imt < 25)
+{
+    kategoriya = "Нормальный вес";
+    sovet = "Так держать!";
+}
+else if (imt < 30)
+{
+    kategoriya = "Избыточный вес";
+    sovet = "Больше двигайтесь";
+}
+else
+{
+    kategoriya = "Ожирение";
+    sovet = "Обратитесь к врачу";
+}
+
+Console.WriteLine($"Ваш ИМТ: {imt}");
+Console.WriteLine($"Категория: {kategoriya}");
+Console.WriteLine($"Совет: {sovet}");
---
+++ code/Task2_BMI/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("H---Введите число---");
+int num = Convert.ToDouble(Console.ReadLine());
+if (num <= 1)
+{
+    Console.WriteLine("Отрицательнок число");
+    return;
+}
+int i = 2;
+while (i * i <= n)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+        i = i + 1;
+    }
+    return true;
